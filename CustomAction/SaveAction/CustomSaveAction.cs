12b18b4f561129da7ce33fb2d36136ec9ce9d61d
sabor413 <phiota413@gmail.com>
2015-10-07 10:30:23 -0400

    Set-up Save Action to edit item in master and publish to web

--- CustomAction/SaveAction/CustomSaveAction.cs
+++ CustomAction/SaveAction/CustomSaveAction.cs
@@ -1,24 +1,29 @@
-﻿using Sitecore.SecurityModel;
-
-namespace CustomAction.SaveAction
+﻿namespace CustomAction.SaveAction
 {
+    using Sitecore.Globalization;
+    using Sitecore.Publishing;
+    using Sitecore.SecurityModel;
     using Sitecore.Data;
     using Sitecore.Form.Submit;
 
     public class CustomAction : ISaveAction
     {
-        public void Execute(Sitecore.Data.ID formid, Sitecore.Form.Core.Client.Data.Submit.AdaptedResultList fields, params object[] data)
+        public void Execute(Sitecore.Data.ID formid, Sitecore.Form.Core.Client.Data.Submit.AdaptedResultList fields,
+            params object[] data)
         {
-            //throw new NotImplementedException();
-
             var master = Sitecore.Configuration.Factory.GetDatabase("master");
+
             var producttwo = master.GetItem(new ID("{2D9304C0-9CA7-484A-BD45-4AB664AFAFE3}"));
             using (new SecurityDisabler())
             {
                 producttwo.Editing.BeginEdit();
-                producttwo.Fields["Price"].Value = "20.00";
+                producttwo.Fields["Price"].Value = "$20.00";
                 producttwo.Editing.EndEdit();
             }
+
+            var web = new[] {Sitecore.Configuration.Factory.GetDatabase("web")};
+            var english = new[] {Language.Current};
+            PublishManager.PublishItem(producttwo, web, english, false, false);
         }
     }
 }
