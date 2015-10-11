9d97191cf61c02f5960f7a09177c1d981a14ecab
sabor413 <phiota413@gmail.com>
2015-10-10 21:01:13 -0400

    Updated to Product Display to demonstrate datasource information and rendering parameters

--- sc80wffmex/Models/Product.cs
+++ sc80wffmex/Models/Product.cs
@@ -14,12 +14,14 @@ namespace sc80wffmex.Models
         public HtmlString Description { get { return new HtmlString(FieldRenderer.Render(Item, "Description")); } }
         public HtmlString Price { get { return new HtmlString(FieldRenderer.Render(Item, "Price")); } }
         public NameValueCollection Parameters { get { return WebUtil.ParseUrlParameters(RenderingContext.Current.Rendering["Parameters"]); } }
-        public Item CurrentItem { get { return PageItem; } } 
+        public Item CurrentItem { get { return PageItem; } }
 
+        #region COMMENTED OUT PROPERTIES
         //public HtmlString MainImage { get { return new HtmlString(FieldRenderer.Render(Item, "SomeImageField")); } }
         //public HtmlString MainDate { get { return new HtmlString(FieldRenderer.Render(Item, "SomeDateField")); } }
         //public string ProductURL { get { return LinkManager.GetItemUrl(Item); } }
         //public string ProductId { get { return Item.ID.ToString(); } }
         //public Item productItem { get { return Item; } }
+        #endregion
     }
 }
