d39d44a9ce7e2f173d42a23b58f08e9353a98a23
sabor413 <phiota413@gmail.com>
2015-10-19 18:15:38 -0400

    Came up with a way to register all controllers automatically, not very elegant but it gets the job done.  If I follow a naming convention for the assembly, that can help focus the controller collection to only those assemblies...

--- sc80wffmexviews/WindsorIoC/ApplicationCastleInstaller.cs
+++ sc80wffmexviews/WindsorIoC/ApplicationCastleInstaller.cs
@@ -1,6 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Reflection;
 using System.Web.Mvc;
+using Castle.Core.Resource;
 
 namespace sc80wffmexviews.WindsorIoC
 {
@@ -19,25 +21,55 @@ namespace sc80wffmexviews.WindsorIoC
             container.Register(Component.For<IControllerSitecoreContext>().ImplementedBy<ContextSitecoreContext>().LifestylePerWebRequest());
 
              // Register the MVC controllers one by one
-            container.Register(Component.For(typeof (MenuController)).LifestylePerWebRequest());
-            container.Register(Component.For(typeof(ProductController)).LifestylePerWebRequest());
+            //container.Register(Component.For(typeof (MenuController)).LifestylePerWebRequest());
+            //container.Register(Component.For(typeof(ProductController)).LifestylePerWebRequest());
 
-            // Register all the MVC controllers in the current executing assembly
-            //var controllers =
-            //    from t in Assembly.GetCallingAssembly().GetTypes()
-            //    where typeof(IController).IsAssignableFrom(t)
-            //    select t;
+            // MVC Controllers are found and registered - Logging it
+            using (var file =
+                new System.IO.StreamWriter(@"C:\Users\Carlos\Documents\Projects\sc80wffmex\Info.txt", true))
+            {
+                foreach (
+                    Assembly b in
+                        AppDomain.CurrentDomain.GetAssemblies().Where(x => x.GetName().ToString().Contains("sc80")))
+                {
+                    file.WriteLine("Found Assembly = " + b.GetName());
 
-            //foreach (Type t in controllers)
-            //    container.AddComponentLifeStyle(
-            //        t.FullName, t, LifestyleType.Transient);
+                    var controllers = b.GetTypes().Where(x => x.BaseType == typeof (Controller)).ToList();
 
-            //var controllers = Assembly.GetExecutingAssembly().GetTypes().Where(x => x.BaseType == typeof(Controller)).ToList();
-            //foreach (var controller in controllers)
-            //{
-            //    container.Register(Component.For(controller).LifestylePerWebRequest());
+                    foreach (var controller in controllers)
+                    {
+                        container.Register(Component.For(controller).LifestylePerWebRequest());
 
-            //}
+                        file.WriteLine("Found Controller = " + controller.Name);
+                    }
+                }
+
+                #region Commented Out Code
+                //var controllers =
+                //    Assembly.GetReferencedAssemblies().GetTypes().Where(t => typeof(IController).IsAssignableFrom(t));
+
+                //foreach (Type t in controllers)
+                //    container.AddComponentLifeStyle(
+                //        t.FullName, t, LifestyleType.Transient);
+
+                // Register all the MVC controllers in the current executing assembly
+                //var controllers =
+                //    from t in Assembly.GetCallingAssembly().GetTypes()
+                //    where typeof(IController).IsAssignableFrom(t)
+                //    select t;
+
+                //foreach (Type t in controllers)
+                //    container.AddComponentLifeStyle(
+                //        t.FullName, t, LifestyleType.Transient);
+
+                //var controllers = Assembly.GetExecutingAssembly().GetTypes().Where(x => x.BaseType == typeof(Controller)).ToList();
+                //foreach (var controller in controllers)
+                //{
+                //    container.Register(Component.For(controller).LifestylePerWebRequest());
+
+                //}
+                #endregion
+            }
         }
     }
 }
